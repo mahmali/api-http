a0b43e78d6ab10f5da2bccd763c0817976887d04
muhammedaliakkaya <muh.ali.akkaya@gmail.com>
2021-04-30 20:49:15 +0300

    v2

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("deneme"))
+	})
+	http.ListenAndServe(":8080", nil)
 }
